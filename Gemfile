f099c05a391da16ec808af5d94752b0e16273ce3
Suvalkietiz <mykolas.tauras@stud.vilniustech.lt>
2025-12-21 17:37:16 +0200

    Part 8-11

--- Gemfile
+++ Gemfile
@@ -69,3 +69,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "devise", "~> 4.9"
+
+gem "tailwindcss-rails", "~> 4.4"
--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,14 @@
 class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_blog_post, except: [:index, :new, :create]
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
+    #@blog_post = BlogPost.find(params[:id])
+  # rescue ActiveRecord::RecordNotFound
+  #   redirect_to root_path
   end
 
   def new
@@ -22,6 +24,32 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def edit
+    #@blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    #@blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    #@blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 
   private
   def blog_post_params
